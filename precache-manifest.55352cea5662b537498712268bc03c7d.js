8f148bc35a9a22ccf58b6e11b6a8dfd5bb174f48
Gleb Vorontsov <g.vorontcov@corp.vk.com>
2019-09-09 17:26:42 +0300

    Updates

---
+++ precache-manifest.55352cea5662b537498712268bc03c7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8cfaf4e29f047533d49726b0b4b97d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "093e5f04701bbb85181c",
+    "url": "/static/css/2.1c828ff2.chunk.css"
+  },
+  {
+    "revision": "093e5f04701bbb85181c",
+    "url": "/static/js/2.8c5521cd.chunk.js"
+  },
+  {
+    "revision": "aba9ea4597d0ca795721",
+    "url": "/static/js/main.a1e5c240.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
